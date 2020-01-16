cb65621288cbe52eae2b73ce8d71254ca12e2c05
Vitor Teixeira de Souza <vitorfox@gmail.com>
2020-01-16 10:11:46 -0300

    Forwading request to grpc server

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"github.com/golang/protobuf/proto"
-	"google.golang.org/grpc/examples/helloworld/helloworld"
 	"log"
 
 	"github.com/buaazp/fasthttprouter"
@@ -18,7 +18,20 @@ func Index(ctx *fasthttp.RequestCtx) {
 
 type Message struct {}
 type Reply struct {
-	Data map[string]string
+	Data []byte
+}
+
+func (m Reply) Reset() {}
+
+func (m Reply) ProtoMessage() {}
+
+func (m Reply) String() string {
+	return fmt.Sprintln(m.Data)
+}
+
+func (m *Reply) XXX_Unmarshal(b []byte) error {
+	m.Data = b
+	return nil
 }
 
 func (m Message) Reset() {}
@@ -29,8 +42,11 @@ func (m Message) String() string {
 	return proto.CompactTextString(m)
 }
 
-func Hello(ctx *fasthttp.RequestCtx) {
-	fmt.Println(ctx, "hello, %s!\n", ctx.UserValue("service"), ctx.UserValue("method"))
+func OptionsProto(ctx *fasthttp.RequestCtx) {
+
+	method := fmt.Sprintf("/proto.%s/%s", ctx.UserValue("service"), ctx.UserValue("method"))
+
+	log.Println(string(ctx.Method()), method)
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 
 	if err != nil {
@@ -38,29 +54,81 @@ func Hello(ctx *fasthttp.RequestCtx) {
 		return
 	}
 
-	client := helloworld.NewGreeterClient(conn)
+	in := Message{}
+	out := Reply{}
 
-	reply, err := client.SayHello(context.Background(), &helloworld.HelloRequest{})
+	err = conn.Invoke(context.Background(), method, &in, &out)
 	
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(out)
+
+	ctx.Response.Header.Set("Access-Control-Allow-Origin","*")
+	ctx.Response.Header.Set("Access-Control-Allow-Methods","GET, POST, OPTIONS")
+	ctx.Response.Header.Set("Access-Control-Allow-Headers","DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Transfer-Encoding,Custom-Header-1,X-Accept-Content-Transfer-Encoding,X-Accept-Response-Streaming,X-User-Agent,X-Grpc-Web")
+	ctx.Response.Header.Set("Access-Control-Max-Age","1728000")
+	ctx.Response.Header.SetContentType("text/plain charset=UTF-8")
+	ctx.Response.Header.SetContentLength(0)
+	ctx.Response.SetStatusCode(204)
+}
+
+func PostProto(ctx *fasthttp.RequestCtx) {
+
+	method := fmt.Sprintf("/proto.%s/%s", ctx.UserValue("service"), ctx.UserValue("method"))
+
+	log.Println(string(ctx.Method()), method)
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(reply)
+	in := Message{}
+	out := Reply{}
 
-	grpc.
-	//err = conn.Invoke(context.Background(), "/proto.Greeter/sayHello", &Message{}, &Reply{})
-	//
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
+	err = conn.Invoke(context.Background(), method, &in, &out)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(out.Data))
+
+	ctx.Response.Header.Reset()
+	ctx.Response.Header.Del("Content-Length")
+	ctx.Response.Header.Set("access-control-allow-origin","*")
+	ctx.Response.Header.Set("access-control-expose-headers","custom-header-1,grpc-status,grpc-message")
+	ctx.Response.Header.SetContentType("application/grpc-web+proto")
+
+	ctx.Response.SetBodyStream(bytes.NewReader(out.Data), len(out.Data))
+
+	/*
+
+	2
+	BTCETHBTC *ETH0
+	BTCUSDTBTC *USDT0grpc-status:0
+	grpc-message:
+
+
+	4
+	2
+	BTCETHBTC *ETH0
+	BTCUSDTBTC *USDT0grpc-status:0
+	grpc-message:
+
+	*/
+
+	//ctx.Response.AppendBodyString("grpc-status:0\ngrpc-message:\n")
 }
 
 func main() {
 	router := fasthttprouter.New()
 	router.GET("/", Index)
-	router.OPTIONS("/proto.:service/:method", Hello)
+	router.OPTIONS("/proto.:service/:method", OptionsProto)
+	router.POST("/proto.:service/:method", PostProto)
 
 	log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
 }
